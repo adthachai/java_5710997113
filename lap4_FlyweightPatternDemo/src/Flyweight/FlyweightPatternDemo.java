5bd2bf6e0aee8e8272cdd0c772942a6bac34373d
adthachai <adthachai1@hotmail.com>
2017-02-11 21:45:22 +0700

    ส่งงาน

---
+++ lap4_FlyweightPatternDemo/src/Flyweight/FlyweightPatternDemo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Flyweight;
+
+/**
+ *
+ * @author BANK
+ */
+public class FlyweightPatternDemo {
+     public static void main(String[] args){}
+     private static String getRandomColor(){return null;}
+     private static int getRandomx(){return 0;}
+     private static int getRandomy(){return 0;}
+}
