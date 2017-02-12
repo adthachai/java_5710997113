eb5787ee140f069cf4682dc48e9e4bec9dd95a3d
adthachai <adthachai1@hotmail.com>
2017-02-12 21:21:31 +0700

    ส่งงาน

---
+++ lap4_CommandPatternDemo/src/Command/Broker.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Command;
+
+import java.util.List;
+import java.util.ArrayList;
+/**
+ *
+ * @author BANK
+ */
+public class Broker {
+    private List<Order> orderList = new ArrayList<Order>(); 
+     public void takeOrder(){}
+     public void placeOrders(){}
+}
